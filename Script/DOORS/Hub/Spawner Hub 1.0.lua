6bdfc0381bbe7319b9bde90489a82f073a427dd9
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-03-09 09:33:03 +0800

    Update script URL for Spawner Hub

--- Script/DOORS/Hub/Spawner Hub 1.0.lua
+++ Script/DOORS/Hub/Spawner Hub 1.0.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/Hub/Source%20Rayfield%20Hub.lua"))()
